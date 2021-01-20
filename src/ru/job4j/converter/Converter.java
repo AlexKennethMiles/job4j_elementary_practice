a2486d846efc56d65779e5859af394649e1c114d
AlexKennethMiles <apuchkov17@gmail.com>
2021-01-20 13:19:44 +0300

    0. Что такое тестирование [#443008]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,36 +2,44 @@ package ru.job4j.converter;
 
 public class Converter {
     public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        int in = 140;
+        int expected = 2;
+        int euro = Converter.rubleToEuro(in);
+        boolean testStatus = euro == expected;
+        System.out.println("140 rubles are " + euro + " euro. Test status : " + testStatus + '.');
         System.out.println("===");
-        int dollar = Converter.rubleToDollar(240);
-        System.out.println("240 rubles are " + dollar + " dollar.");
+        in = 240;
+        expected = 4;
+        int dollar = Converter.rubleToDollar(in);
+        testStatus = dollar == expected;
+        System.out.println("240 rubles are " + dollar + " dollar. Test status : " + testStatus + '.');
         System.out.println("===");
-        int ruble = Converter.dollarToRuble(15);
-        System.out.println("15 dollars are " + ruble + " ruble.");
-        ruble = Converter.euroToRuble(5);
-        System.out.println("5 euro are " + ruble + " ruble.");
+        in = 15;
+        expected = 900;
+        int ruble = Converter.dollarToRuble(in);
+        testStatus = ruble == expected;
+        System.out.println("15 dollars are " + ruble + " ruble. Test status : " + testStatus + '.');
+        in = 5;
+        expected = 350;
+        ruble = Converter.euroToRuble(in);
+        testStatus = ruble == expected;
+        System.out.println("5 euro are " + ruble + " ruble. Test status : " + testStatus + '.');
         // Since we work with int, we will not be able to transfer from dollars to euros and vice versa.
     }
 
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static int rubleToDollar(int value) {
-        int rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static int euroToRuble(int value) {
-        int rsl = value * 70;
-        return rsl;
+        return value * 70;
     }
 
     public static int dollarToRuble(int value) {
-        int rsl = value * 60;
-        return rsl;
+        return value * 60;
     }
 }
