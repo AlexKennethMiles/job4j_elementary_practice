fe3a2d4949926f3f702d0999d1355043a8dc4a62
AlexKennethMiles <apuchkov17@gmail.com>
2021-02-05 15:14:07 +0300

    5.5.(1) Простое число [#443036]

--- test/ru/job4j/loop/CheckPrimeNumberTest.java
+++ test/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -47,4 +47,10 @@ public class CheckPrimeNumberTest {
         boolean rsl = CheckPrimeNumber.check(211);
         assertThat(rsl, is(true));
     }
+
+    @Test
+    public void when1() {
+        boolean rsl = CheckPrimeNumber.check(1);
+        assertThat(rsl, is(false));
+    }
 }
