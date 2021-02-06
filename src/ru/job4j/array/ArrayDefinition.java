123a251e6b41737bb1f37f12667044c81569628c
AlexKennethMiles <apuchkov17@gmail.com>
2021-02-06 13:26:48 +0300

    6.0.3.1. Массивы и цикл for-each [#442967]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -22,8 +22,8 @@ public class ArrayDefinition {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
