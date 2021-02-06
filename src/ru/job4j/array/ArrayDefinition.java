cb2d8702d2f77ff1d7dcf5c0a7f5c2eccd100062
AlexKennethMiles <apuchkov17@gmail.com>
2021-02-06 13:16:05 +0300

    6.0.2.(1) Заполнение массива. [#442957]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -14,8 +14,8 @@ public class ArrayDefinition {
         names[1] = "Alexey Puchkov";
         names[2] = "Ivan Ivanov";
         names[3] = "Alexandr Morozov";
-        for (int i = 0; i < names.length - 1; i++) {
-            System.out.println(names[i]);
+        for (String name : names) {
+            System.out.println(name);
         }
     }
 }
