a3fc813671df6bbc7a0405a1c6469aa8d507cc09
AlexKennethMiles <apuchkov17@gmail.com>
2021-02-20 23:38:47 +0300

    37. Массив без четных значений.

---
+++ src/ru/job4j/array/ArrayWithoutEvenElements.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArrayWithoutEvenElements {
+    public static int[] changeData(int[] data) {
+        int[] rsl = new int[data.length];
+        int count = 0;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] % 2 != 0) {
+                rsl[count] = data[i];
+                count++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, count);
+        return rsl;
+    }
+}
